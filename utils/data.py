a3acc96ebf45ac758b3c85b49de20855b62d3c3a
ChaXxl <x734461204@gmail.com>
2024-07-21 16:48:50 +0800

    feat: 增加数据库连接功能

--- utils/data.py
+++ utils/data.py
@@ -1,3 +1,33 @@
+from pathlib import Path
+
+import psycopg2
+
+from log import Logger
+
+
 class Data():
     def __init__(self):
-        pass
+        # 保存日志的目录
+        self.log_dir = Path('./log')
+        self.log_dir.mkdir(exist_ok=True)
+
+        # 日志, 保存时以时间命名
+        self.logger = Logger(self.log_dir / 'data.log')
+
+        self.conn = None
+        self.cursor = None
+
+        self.connect()
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(
+                user='xxl',
+                password='4444',
+                host='198.19.249.83',
+                port='5432',
+                database='zhipin'
+            )
+            self.cursor = self.conn.cursor()
+        except Exception as e:
+            self.logger.error(f'connect error: {e}')
